1e7d4824e117a649d28001c3378b6a1689d99f2d
Andrew <andrew.rutherford.6@gmail.com>
2020-02-15 14:52:05 +0700

    created Home and NotFound components

---
+++ src/components/pages/NotFound.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const NotFound = () => {
+  return (
+    <div>
+      <h1>
+        {' '}
+        <i className='fas fa-info-circle'></i> Not Found
+      </h1>
+      <p className='lead'>The page you are looking for does not exist</p>
+    </div>
+  );
+};
+
+export default NotFound;
